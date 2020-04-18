0db69c11febf3596b8107a9226554dea91552e8b
Justas Raudonius <justas.raudonius@adaptagency.com>
2020-04-18 21:22:14 +0300

    wip

---
+++ api/index.js
@@ -0,0 +1,28 @@
+// const logger = require('pino')();
+const fs = require('fs');
+
+module.exports = (request, response) => {
+    let responseData = [];
+
+    const dirs = [
+        '.',
+        'dist',
+        'api',
+    ];
+    dirs.forEach((dir) => {
+        console.log(dir);
+        try {
+            responseData.push({
+                "dir": dir,
+                "content": fs.readdirSync(dir),
+            });
+        } catch (e) {
+            responseData.push({
+                "dir": dir,
+                "exception": e.message,
+            });
+        }
+    })
+
+    response.end(JSON.stringify(responseData));
+}
